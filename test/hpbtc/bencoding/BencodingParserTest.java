3d3c69f931fabff3c70dde2c710cb33141127800
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-04-24 14:01:47 +0000

    adding tests

--- test/hpbtc/bencoding/BencodingParserTest.java
+++ test/hpbtc/bencoding/BencodingParserTest.java
@@ -1,5 +1,6 @@
 package hpbtc.bencoding;
 
+import hpbtc.bencoding.element.BencodedInteger;
 import hpbtc.bencoding.element.BencodedString;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -20,7 +21,6 @@ public class BencodingParserTest {
             try {
                 BencodedString string = parser.readNextString();
                 assert string.equals("test") : "String is incorrect";
-                is.close();
             } catch (IOException e) {
                 assert false : e.getMessage();
             }
@@ -29,10 +29,58 @@ public class BencodingParserTest {
         }
     }
 
-    @Test
-    public void testReadNextInteger() {
+    @Test(expected = hpbtc.bencoding.BencodingException.class)
+    public void testReadNextStringWithWrongLength() throws IOException {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream("4a:test".getBytes("ISO-8859-1"));
+            BencodingParser parser = new BencodingParser(is);
+            parser.readNextString();
+            assert false : "String is incorrect";
+        } catch (UnsupportedEncodingException e) {
+            assert false : e.getMessage();
+        }
     }
 
+    @Test
+    public void testReadNextInteger() {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream("i89e".getBytes("ISO-8859-1"));
+            BencodingParser parser = new BencodingParser(is);
+            try {
+                BencodedInteger i = parser.readNextInteger();
+                assert i.getValue() == 89 : "Integer is incorrect";
+            } catch (IOException e) {
+                assert false : e.getMessage();
+            }
+        } catch (UnsupportedEncodingException e) {
+            assert false : e.getMessage();
+        }
+    }
+
+    @Test(expected = hpbtc.bencoding.BencodingException.class)
+    public void testReadNextIntegerWithoutEnding() throws IOException {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream("i89".getBytes("ISO-8859-1"));
+            BencodingParser parser = new BencodingParser(is);
+            parser.readNextInteger();
+            assert false : "Integer is incorrect";
+        } catch (UnsupportedEncodingException e) {
+            assert false : e.getMessage();
+        }
+    }
+
+    @Test(expected = hpbtc.bencoding.BencodingException.class)
+    public void testReadNextIntegerWithZeroPrefix() throws IOException {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream("i05e".getBytes("ISO-8859-1"));
+            BencodingParser parser = new BencodingParser(is);
+            parser.readNextInteger();
+            assert false : "Integer is incorrect";
+        } catch (UnsupportedEncodingException e) {
+            assert false : e.getMessage();
+        }
+    }
+    
     @Test
     public void testReadNextList() {
     }
