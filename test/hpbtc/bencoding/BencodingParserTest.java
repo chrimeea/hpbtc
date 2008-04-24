bdfd4085218e73d95de6aa469c49a749c31b6a67
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-04-24 13:34:03 +0000

    adding tests

--- test/hpbtc/bencoding/BencodingParserTest.java
+++ test/hpbtc/bencoding/BencodingParserTest.java
@@ -1,19 +1,12 @@
 package hpbtc.bencoding;
 
 import org.junit.Test;
-import org.junit.Before;
 
 /**
  *
  * @author Cristian Mocanu
  */
 public class BencodingParserTest {
-
-    private BencodingParser parser;
-    
-    @Before
-    public void setUp() {
-    }
     
     @Test
     public void testReadNextString() {
