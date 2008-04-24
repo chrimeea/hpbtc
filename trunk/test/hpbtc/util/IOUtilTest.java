a9721f55b050f608f6bf7fe8023e961e868e8ce1
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-04-24 07:27:43 +0000

    adding tests

--- trunk/test/hpbtc/util/IOUtilTest.java
+++ trunk/test/hpbtc/util/IOUtilTest.java
@@ -1,17 +1,43 @@
 package hpbtc.util;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import org.junit.Test;
+import org.junit.Before;
+import static org.easymock.EasyMock.*;
 
 /**
  *
  * @author Cristian Mocanu
  */
 public class IOUtilTest {
-    
+
+    private ReadableByteChannel mockReadable;
+    private WritableByteChannel mockWritable;
+
+    @Before
+    public void setUp() {
+        mockReadable = createMock(ReadableByteChannel.class);
+        mockWritable = createMock(WritableByteChannel.class);
+    }
+
     @Test
     public void testReadFromChannel() {
+        ByteBuffer b = ByteBuffer.allocate(1);
+        try {
+            expect(mockReadable.read(b)).andReturn(-1);
+            replay(mockReadable);
+            int rep = IOUtil.readFromChannel(mockReadable, b);
+            assert rep == 0;
+            assert b.remaining() == 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            assert false;
+        }
     }
-        
+
     @Test
     public void testWriteToChannel() {
     }
