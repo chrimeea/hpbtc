3a8fadeb4a3154949a40944c8d9b7a5e6d875bfe
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-04-24 13:44:31 +0000

    adding tests

--- test/hpbtc/bencoding/BencodingParserTest.java
+++ test/hpbtc/bencoding/BencodingParserTest.java
@@ -1,5 +1,9 @@
 package hpbtc.bencoding;
 
+import hpbtc.bencoding.element.BencodedString;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import org.junit.Test;
 
 /**
@@ -7,23 +11,36 @@ import org.junit.Test;
  * @author Cristian Mocanu
  */
 public class BencodingParserTest {
-    
+
     @Test
     public void testReadNextString() {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream("4:test".getBytes("ISO-8859-1"));
+            BencodingParser parser = new BencodingParser(is);
+            try {
+                BencodedString string = parser.readNextString();
+                assert string.equals("test") : "String is incorrect";
+                is.close();
+            } catch (IOException e) {
+                assert false : e.getMessage();
+            }
+        } catch (UnsupportedEncodingException e) {
+            assert false : e.getMessage();
+        }
     }
-    
+
     @Test
     public void testReadNextInteger() {
     }
-    
+
     @Test
     public void testReadNextList() {
     }
-    
+
     @Test
     public void readNextDictionary() {
     }
-    
+
     @Test
     public void readNextElement() {
     }
