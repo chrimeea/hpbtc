1e10667c98e4e19f9826873f518713aa2d211b01
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-04-25 08:07:32 +0000

    adding tests

--- src/hpbtc/bencoding/element/BencodedElement.java
+++ src/hpbtc/bencoding/element/BencodedElement.java
@@ -6,15 +6,12 @@ package hpbtc.bencoding.element;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Logger;
 
 /**
  * @author chris
  *
  */
 public abstract class BencodedElement {
-
-    private static Logger logger = Logger.getLogger(BencodedElement.class.getName());
 
     /**
      * @return
