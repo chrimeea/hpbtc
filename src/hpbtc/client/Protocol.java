0cd3ef87614cb47b03e04e08742f3a0b24e07dce
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-05-12 14:12:44 +0000

    tests

---
+++ src/hpbtc/client/Protocol.java
@@ -0,0 +1,54 @@
+package hpbtc.client;
+
+import hpbtc.protocol.network.*;
+import hpbtc.protocol.torrent.Peer;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+/**
+ *
+ * @author Cristian Mocanu
+ */
+public class Protocol {
+
+    private Client client;
+    private Map<InetSocketAddress, ClientMessageProcessor> peers;
+
+    public Protocol(Client client) {
+        this.client = client;
+    }
+
+    public void startProtocol() {
+        new Thread(new Runnable() {
+
+            public void run() {
+                synchronized (client) {
+                    do {
+                        do {
+                            try {
+                                client.wait();
+                            } catch (InterruptedException e) {
+                            }
+                        } while (!client.hasUnreadMessages());
+                        process(client.takeMessage());
+                    } while (client.hasUnreadMessages());
+                }
+            }
+        }).start();
+    }
+
+    private void process(ClientProtocolMessage data) {
+        ClientMessageProcessor processor = peers.get(data.getPeer());
+        if (processor == null) {
+            processor = new ClientMessageProcessor(client, data.getPeer());
+            peers.put(data.getPeer(), processor);
+        }
+        processor.process(data.getMessage());
+    }
+    
+    public void connect(Peer peer) {
+        ClientMessageProcessor processor = new ClientMessageProcessor(client, peer);
+        peers.put(peer.getAddress(), processor);
+        processor.connect();
+    }
+}
