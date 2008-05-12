c349effa1358638a03175684357cc0d42a7ebe82
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-05-12 14:13:29 +0000

    protocol

---
+++ trunk/src/hpbtc/client/ClientMessageProcessor.java
@@ -0,0 +1,77 @@
+package hpbtc.client;
+
+import hpbtc.protocol.network.*;
+import hpbtc.protocol.torrent.Peer;
+import hpbtc.protocol.message.MessageProcessor;
+import hpbtc.protocol.message.BitfieldMessage;
+import hpbtc.protocol.message.CancelMessage;
+import hpbtc.protocol.message.ChokeMessage;
+import hpbtc.protocol.message.HandshakeMessage;
+import hpbtc.protocol.message.HaveMessage;
+import hpbtc.protocol.message.IdleMessage;
+import hpbtc.protocol.message.InterestedMessage;
+import hpbtc.protocol.message.NotInterestedMessage;
+import hpbtc.protocol.message.PIDMessage;
+import hpbtc.protocol.message.PieceMessage;
+import hpbtc.protocol.message.RequestMessage;
+import hpbtc.protocol.message.UnchokeMessage;
+import java.net.InetSocketAddress;
+
+/**
+ *
+ * @author Cristian Mocanu
+ */
+public class ClientMessageProcessor implements MessageProcessor {
+
+    private Client client;
+    
+    public ClientMessageProcessor(Client client, Peer messagePeer) {
+        this.client = client;
+    }
+    
+    public ClientMessageProcessor(Client client, InetSocketAddress address) {
+        this.client = client;
+    }
+
+    public void process(byte[] data) {
+    }
+    
+    public void connect() {
+    }
+    
+    public void process(BitfieldMessage message) {
+    }
+
+    public void process(CancelMessage message) {
+    }
+
+    public void process(ChokeMessage message) {
+    }
+
+    public void process(HandshakeMessage message) {
+    }
+
+    public void process(HaveMessage message) {
+    }
+
+    public void process(IdleMessage message) {
+    }
+
+    public void process(InterestedMessage message) {
+    }
+
+    public void process(NotInterestedMessage message) {
+    }
+
+    public void process(PIDMessage message) {
+    }
+
+    public void process(final PieceMessage message) {
+    }
+
+    public void process(RequestMessage message) {
+    }
+
+    public void process(UnchokeMessage message) {
+    }
+}
