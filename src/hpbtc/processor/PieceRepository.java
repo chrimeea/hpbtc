90178e65dbd3594493f5cab64e317f8665745b8e
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-05-30 13:45:44 +0000

    piecerep

--- src/hpbtc/processor/PieceRepository.java
+++ src/hpbtc/processor/PieceRepository.java
@@ -1,13 +1,9 @@
 package hpbtc.processor;
 
-import hpbtc.protocol.torrent.BTFile;
-import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import hpbtc.protocol.torrent.Peer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -15,41 +11,19 @@ import java.util.List;
  */
 public class PieceRepository {
 
-    public static final int DEFAULT_CHUNK = 16384;
-    private List<BTFile> files;
-
-    public PieceRepository(List<BTFile> files) {
-        this.files = files;
+    Map<byte[], Map<Long, List<Peer>>> pieces;
+    
+    public PieceRepository() {
+        pieces = new HashMap<byte[], Map<Long, List<Peer>>>();
     }
 
+    /*
     private boolean checkHash(ByteBuffer bb, byte[] hash) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA1");
         bb.rewind();
         md.update(bb);
         return Arrays.equals(md.digest(), hash);
     }
-
-    /**
-     * @param begin
-     * @param length
-     * @return
-     * @throws IOException
      */
-    public ByteBuffer getPiece(int begin, int length) throws IOException {
-        return null;
-    }
 
-    /**
-     * @param o
-     * @param b
-     * @return
-     */
-    public void savePiece(int begin, ByteBuffer b) {
-    }
-
-    private void createFile(String path) throws IOException {
-        File f = new File(path);
-        f.mkdirs();
-        f.createNewFile();
-    }
 }
