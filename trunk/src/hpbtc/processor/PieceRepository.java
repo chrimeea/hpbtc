2a5cb35439a3fa2bfb9763c316bd140c9270ed99
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-05-30 14:05:31 +0000

    pieces

--- trunk/src/hpbtc/processor/PieceRepository.java
+++ trunk/src/hpbtc/processor/PieceRepository.java
@@ -1,22 +1,14 @@
 package hpbtc.processor;
 
-import hpbtc.protocol.torrent.Peer;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Map;
-
 /**
  *
  * @author Cristian Mocanu
  */
 public class PieceRepository {
-
-    Map<byte[], Map<Long, Set<Peer>>> pieces;
     
     public PieceRepository() {
-        pieces = new HashMap<byte[], Map<Long, Set<Peer>>>();
     }
-
+    
     /*
     private boolean checkHash(ByteBuffer bb, byte[] hash) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA1");
