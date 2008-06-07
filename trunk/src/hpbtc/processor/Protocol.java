5a0a01454008b7bcd0df3678781b7ce496cda709
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-06-07 19:41:29 +0000

    refactoring protocol

--- trunk/src/hpbtc/processor/Protocol.java
+++ trunk/src/hpbtc/processor/Protocol.java
@@ -16,8 +16,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -32,26 +30,16 @@ public class Protocol {
     private static Logger logger = Logger.getLogger(Protocol.class.getName());
     private Map<byte[], Torrent> torrents;
     private Network network;
-    private byte[] peerId;
-    private PieceRepository pieceRep;
+    private MessageValidator validator;
+    private MessageProcessor processor;
 
-    public Protocol() {
+    public Protocol() throws UnsupportedEncodingException {
         torrents = new HashMap<byte[], Torrent>();
         network = new Network();
-        pieceRep = new PieceRepository();
-        generateId();
-    }
-
-    private void generateId() {
-        peerId = new byte[20];
-        peerId[0] = 'C';
-        peerId[1] = 'M';
-        peerId[2] = '-';
-        peerId[3] = '2';
-        peerId[4] = '.';
-        peerId[5] = '0';
-        Random r = new Random();
-        r.nextBytes(peerId);
+        PieceRepository pieceRep = new PieceRepository();
+        byte[] protocol = getSupportedProtocol();
+        validator = new MessageValidator(torrents, protocol, pieceRep);
+        processor = new MessageProcessor(network, protocol, pieceRep);
     }
 
     public void download(String fileName) throws IOException, NoSuchAlgorithmException {
@@ -117,58 +105,55 @@ public class Protocol {
                     switch (disc) {
                         case SimpleMessage.TYPE_BITFIELD:
                             BitfieldMessage mBit = new BitfieldMessage(current, len);
-                            processBitfield(mBit.getBitfield(), len, peer);
+                            if (validator.validateBitfieldMessage(mBit, peer)) {
+                                processor.processBitfield(mBit, peer);
+                            }
                             break;
                         case SimpleMessage.TYPE_CANCEL:
                             BlockMessage mCan = new BlockMessage(current, SimpleMessage.TYPE_CANCEL);
-                            processCancel(mCan.getBegin(), mCan.getIndex(), mCan.getLength(), peer);
+                            if (validator.validateCancelMessage(mCan, peer)) {
+                                processor.processCancel(mCan, peer);
+                            }
                             break;
                         case SimpleMessage.TYPE_CHOKE:
-                            processChoke(peer);
+                            processor.processChoke(peer);
                             break;
                         case SimpleMessage.TYPE_HAVE:
                             HaveMessage mHave = new HaveMessage(current);
-                            processHave(mHave.getIndex(), peer);
+                            if (validator.validateHaveMessage(mHave, peer)) {
+                                processor.processHave(mHave, peer);
+                            }
                             break;
                         case SimpleMessage.TYPE_INTERESTED:
-                            processInterested(peer);
+                            processor.processInterested(peer);
                             break;
                         case SimpleMessage.TYPE_NOT_INTERESTED:
-                            processNotInterested(peer);
+                            processor.processNotInterested(peer);
                             break;
                         case SimpleMessage.TYPE_PIECE:
                             PieceMessage mPiece = new PieceMessage(current, len);
-                            processPiece(mPiece.getBegin(), mPiece.getIndex(), mPiece.getPiece(), peer);
+                            if (validator.validatePieceMessage(mPiece, peer)) {
+                                processor.processPiece(mPiece, peer);
+                            }
                             break;
                         case SimpleMessage.TYPE_REQUEST:
                             BlockMessage mReq = new BlockMessage(current, SimpleMessage.TYPE_REQUEST);
-                            processRequest(mReq.getBegin(), mReq.getIndex(), mReq.getLength(), peer);
+                            if (validator.validateRequestMessage(mReq, peer)) {
+                                processor.processRequest(mReq, peer);
+                            }
                             break;
                         case SimpleMessage.TYPE_UNCHOKE:
-                            processUnchoke(peer);
+                            processor.processUnchoke(peer);
                     }
                     peer.setMessagesReceived();
                 }
-            } else {
-                if (current.remaining() < 48) {
-                    throw new EOFException("wrong hanshake");
-                }
+            } else if (current.remaining() >= 48) {
                 HandshakeMessage mHand = new HandshakeMessage(current);
-                processHandshake(mHand.getProtocol(), peer, mHand.getPeerId(), mHand.getInfoHash());
+                if (validator.validateHandshakeMessage(mHand, peer)) {
+                    processor.processHandshake(mHand, peer);
+                }
             }
         } while (current.remaining() > 0);
-    }
-
-    private void processHandshake(byte[] protocol, Peer peer, byte[] pid, byte[] infoHash) throws IOException {
-        if (Arrays.equals(protocol, getSupportedProtocol()) && torrents.containsKey(infoHash)) {
-            HandshakeMessage reply = new HandshakeMessage(infoHash, peerId, protocol);
-            network.postMessage(peer, reply);
-        } else {
-            throw new IOException("wrong message");
-        }
-        peer.setId(pid);
-        peer.setInfoHash(infoHash);
-        peer.setHandshakeReceived();
     }
 
     private byte[] getSupportedProtocol() throws UnsupportedEncodingException {
@@ -177,74 +162,5 @@ public class Protocol {
         pr.put((byte) 19);
         pr.put("BitTorrent protocol".getBytes("US-ASCII"));
         return protocol;
-    }
-    
-    private void processBitfield(BitSet pieces, int len, Peer peer) throws IOException {
-        if (peer.isMessagesReceived()) {
-            throw new IOException("wrong message");
-        } else {
-            long nrPieces = torrents.get(peer.getInfoHash()).getNrPieces();
-            if (pieces.length() > nrPieces || len != Math.ceil(nrPieces / 8.0)) {
-                throw new IOException("wrong message");
-            }
-        }
-        peer.setPieces(pieces);
-    }
-
-    private void processCancel(int begin, int index, int length, Peer peer) throws IOException {
-        Torrent t = torrents.get(peer.getInfoHash());
-        if (index >= t.getNrPieces() ||
-            begin >= t.getPieceLength()) {
-            throw new IOException("wrong message");
-        }
-        network.cancelPieceMessage(begin, index, length, peer);
-    }
-
-    private void processChoke(Peer peer) {
-        peer.setPeerChoking(true);
-    }
-
-    private void processHave(int index, Peer peer) throws IOException {
-        Torrent t = torrents.get(peer.getInfoHash());
-        if (index >= t.getNrPieces()) {
-            throw new IOException("wrong message");
-        }
-        peer.setPiece(index);
-    }
-
-    private void processInterested(Peer peer) {
-        peer.setPeerInterested(true);
-    }
-
-    private void processNotInterested(Peer peer) {
-        peer.setPeerInterested(false);
-    }
-
-    private void processPiece(int begin, int index, ByteBuffer piece, Peer peer) throws IOException {
-        byte[] infoHash = peer.getInfoHash();
-        Torrent t = torrents.get(infoHash);
-        if (index >= t.getNrPieces() ||
-            begin >= t.getPieceLength()) {
-            throw new IOException("wrong message");
-        }
-        pieceRep.savePiece(infoHash, begin, index, piece);
-    }
-    
-    private void processRequest(int begin, int index, int length, Peer peer) throws IOException {
-        byte[] infoHash = peer.getInfoHash();
-        Torrent t = torrents.get(infoHash);
-        long pieceLength = t.getPieceLength();
-        if (index >= t.getNrPieces() ||
-            begin >= pieceLength || begin + length > pieceLength ||
-            !pieceRep.isPiece(infoHash, index)) {
-            throw new IOException("wrong message");
-        }
-        ByteBuffer piece = pieceRep.getPiece(infoHash, begin, index, length);
-        PieceMessage pm = new PieceMessage(begin, index, piece, length);
-        network.postMessage(peer, pm);
-    }
-
-    private void processUnchoke(Peer peer) {
-        peer.setPeerChoking(false);
     }
 }
