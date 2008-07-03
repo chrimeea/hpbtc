32d8320115ecbcc0a66113c8b4bdda972541b06d
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-07-03 08:42:36 +0000

    test save piece

--- test/hpbtc/protocol/torrent/TorrentTest.java
+++ test/hpbtc/protocol/torrent/TorrentTest.java
@@ -1,12 +1,16 @@
 package hpbtc.protocol.torrent;
 
+import hpbtc.bencoding.BencodingWriter;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 import java.util.List;
 import org.junit.Test;
+import util.IOUtil;
 
 /**
  *
@@ -18,6 +22,7 @@ public class TorrentTest {
     public void testTorrentInfo() throws IOException, NoSuchAlgorithmException {
         ByteArrayInputStream b = new ByteArrayInputStream("d8:announce27:http://www.test.ro/announce7:comment12:test comment10:created by13:uTorrent/177013:creation datei1209116668e8:encoding5:UTF-84:infod6:lengthi85e4:name11:manifest.mf12:piece lengthi65536e6:pieces20:12345678901234567890ee".getBytes("UTF-8"));
         Torrent info = new Torrent(b, ".");
+        b.close();
         assert info.getFileLength() == 85;
         assert info.getComment().equals("test comment");
         assert info.getCreatedBy().equals("uTorrent/1770");
@@ -35,5 +40,35 @@ public class TorrentTest {
         BTFile f = files.get(0);
         assert f.getLength() == 85;
         assert f.getPath().equals(".\\manifest.mf");
+    }
+    
+    @Test
+    public void testSavePiece() throws IOException, NoSuchAlgorithmException {
+        File f = File.createTempFile("hpbtc-save-piece", "test");
+        String t = "d8:announce27:http://www.test.ro/announce7:comment12:test comment10:created by13:uTorrent/177013:creation datei1209116668e8:encoding5:UTF-84:infod6:lengthi85e4:name";
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        BencodingWriter wr = new BencodingWriter(bos);
+        wr.write(f.getName());
+        String s = new String(bos.toByteArray(), "UTF-8");
+        t += s + "12:piece lengthi65536e6:pieces20:12345678901234567890ee";        
+        bos.close();
+        ByteArrayInputStream b = new ByteArrayInputStream(t.getBytes("UTF-8"));
+        Torrent info = new Torrent(b, f.getParent());
+        b.close();
+        ByteBuffer piece = ByteBuffer.allocate(info.getPieceLength());
+        for (int i = 0; i < info.getPieceLength(); i++) {
+            piece.put((byte) 5);
+        }
+        piece.rewind();
+        info.savePiece(0, 0, piece);
+        ByteBuffer dest = ByteBuffer.allocate(info.getPieceLength());
+        IOUtil.readFromFile(f, 0, dest);
+        dest.rewind();
+        piece.rewind();
+        assert dest.equals(piece);
+    }
+    
+    @Test
+    public void testLoadPiece() {
     }
 }
