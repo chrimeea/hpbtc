3f6666e1b0eeb62ca29059c76bf35d983c02c1f2
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-07-13 15:57:35 +0000

    hash

--- src/util/TorrentUtil.java
+++ src/util/TorrentUtil.java
@@ -1,11 +1,8 @@
 package util;
 
-import hpbtc.bencoding.BencodingWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Map;
 
 /**
  *
@@ -16,22 +13,19 @@ public class TorrentUtil {
     public static int computeBeginIndex(int begin, int chunkSize) {
         return begin / chunkSize;
     }
-    
+
     public static int computeEndIndex(int begin, int length, int chunkSize) {
         return 1 + (begin + length) / chunkSize;
     }
-   
+
     public static int computeBeginPosition(int begin, int chunkSize) {
         return begin * chunkSize;
     }
-    
-    public static byte[] computeInfoHash(Map<String, Object> info)
-            throws NoSuchAlgorithmException, IOException {
+
+    public static byte[] computeInfoHash(byte[] info)
+            throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA1");
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        BencodingWriter w = new BencodingWriter(os);
-        w.write(info);
-        md.update(os.toByteArray());
+        md.update(info);
         return md.digest();
     }
 }
