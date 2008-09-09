a74000a8965168813338b6257b0be88d7e7425c1
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-09-09 16:48:01 +0000

    fix peer infohash

--- src/util/TrackerUtil.java
+++ src/util/TrackerUtil.java
@@ -17,8 +17,9 @@ import java.util.Set;
  */
 public class TrackerUtil {
 
-    public static Set<Peer> doCompactPeer(Map<String, Object> response) throws
-            UnsupportedEncodingException, UnknownHostException {
+    public static Set<Peer> doCompactPeer(Map<String, Object> response,
+            byte[] infoHash) throws UnsupportedEncodingException,
+            UnknownHostException {
         Set<Peer> peers = new HashSet<Peer>();
         byte[] prs = ((String) response.get("peers")).getBytes("ISO-8859-1");
         int k = 0;
@@ -28,20 +29,22 @@ public class TrackerUtil {
             k += 6;
             int peerPort = getUnsigned(prs[k - 2]) * 256 + getUnsigned(
                     prs[k - 1]);
-            peers.add(new Peer(new InetSocketAddress(peerIp, peerPort), null));
+            peers.add(new Peer(new InetSocketAddress(peerIp, peerPort), infoHash,
+                    null));
         }
         return peers;
     }
 
-    public static Set<Peer> doLoosePeer(Map<String, Object> response) throws
+    public static Set<Peer> doLoosePeer(Map<String, Object> response,
+            byte[] infoHash) throws
             UnsupportedEncodingException {
         Set<Peer> peers = new HashSet<Peer>();
         List<Map<String, Object>> prs =
                 (List<Map<String, Object>>) response.get("peers");
         for (Map<String, Object> d : prs) {
             peers.add(new Peer(new InetSocketAddress((String) d.get("ip"),
-                    ((Integer) d.get("port")).intValue()), ((String) d.get(
-                    "peer id")).getBytes("ISO-8859-1")));
+                    ((Integer) d.get("port")).intValue()), infoHash,
+                    ((String) d.get("peer id")).getBytes("ISO-8859-1")));
         }
         return peers;
     }
