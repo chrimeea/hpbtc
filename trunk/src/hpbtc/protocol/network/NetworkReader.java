171d4985da8e74b1c67a002c09072a1684e271cb
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-09-13 11:13:41 +0000

    fix register

--- trunk/src/hpbtc/protocol/network/NetworkReader.java
+++ trunk/src/hpbtc/protocol/network/NetworkReader.java
@@ -128,8 +128,8 @@ public class NetworkReader {
                         }
                     }
                 }
-                register.performReadRegistration();
             }
+            register.performReadRegistration();
         }
     }
 }
