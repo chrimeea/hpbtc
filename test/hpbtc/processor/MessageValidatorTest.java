2e8762825c9cca348f9dea5368fe2647e11aa908
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-09-25 13:21:26 +0000

    tests

--- test/hpbtc/processor/MessageValidatorTest.java
+++ test/hpbtc/processor/MessageValidatorTest.java
@@ -4,6 +4,7 @@
 
 package hpbtc.processor;
 
+import hpbtc.protocol.message.BitfieldMessage;
 import hpbtc.protocol.message.HandshakeMessage;
 import hpbtc.protocol.torrent.Peer;
 import hpbtc.protocol.torrent.Torrent;
@@ -13,7 +14,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -23,6 +26,16 @@ import org.junit.Test;
 public class MessageValidatorTest {
 
     private String encoding = "US-ASCII";
+    private byte[] protocol;
+    
+    @Before
+    public void before() {
+        try {
+            protocol = "PROT".getBytes(encoding);
+        } catch (UnsupportedEncodingException ex) {
+            assert false;
+        }
+    }
     
     @Test
     public void testValidateHandshakeMessage() throws
@@ -33,17 +46,29 @@ public class MessageValidatorTest {
         Torrent info = new Torrent(b, ".", null, 0);
         b.close();
         List<Torrent> t = new ArrayList<Torrent>(1);
-        byte[] p = "PROT".getBytes(encoding);
-        HandshakeMessage hm = new HandshakeMessage(null, p, peer,
+        HandshakeMessage hm = new HandshakeMessage(null, protocol, peer,
                 info.getInfoHash());
-        MessageValidator v = new MessageValidator(t, p);
+        MessageValidator v = new MessageValidator(t, protocol);
         assert !v.validateHandshakeMessage(hm);
         t.add(info);
         assert v.validateHandshakeMessage(hm);
     }
     
     @Test
-    public void testValidateBitfieldMessage() {
+    public void testValidateBitfieldMessage() throws
+            UnsupportedEncodingException, IOException, NoSuchAlgorithmException {
+        Peer peer = new Peer(InetSocketAddress.createUnresolved("localhost",
+                6000), null);
+        ByteArrayInputStream b = new ByteArrayInputStream("d8:announce27:http://www.test.ro/announce7:comment12:test comment10:created by13:uTorrent/177013:creation datei1209116668e8:encoding5:UTF-84:infod6:lengthi85e4:name11:manifest.mf12:piece lengthi65536e6:pieces20:12345678901234567890ee".getBytes(encoding));
+        Torrent info = new Torrent(b, ".", null, 0);
+        b.close();
+        peer.setTorrent(info);
+        BitSet bs = new BitSet(info.getNrPieces());
+        BitfieldMessage bm = new BitfieldMessage(bs, info.getNrPieces(), peer);
+        List<Torrent> t = new ArrayList<Torrent>(1);
+        t.add(info);
+        MessageValidator v = new MessageValidator(t, protocol);
+        assert v.validateBitfieldMessage(bm);
     }
     
     @Test
