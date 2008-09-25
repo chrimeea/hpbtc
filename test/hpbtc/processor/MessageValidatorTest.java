0eae7c66c19ca022ede02742798bf1c87fa42b17
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-09-25 14:20:11 +0000

    tests

--- test/hpbtc/processor/MessageValidatorTest.java
+++ test/hpbtc/processor/MessageValidatorTest.java
@@ -127,6 +127,17 @@ public class MessageValidatorTest {
     }
     
     @Test
-    public void testValidateRequestMessage() {
+    public void testValidateRequestMessage() throws
+            UnsupportedEncodingException, IOException, NoSuchAlgorithmException {
+        Peer peer = new Peer(InetSocketAddress.createUnresolved("localhost",
+                6000), null);
+        ByteArrayInputStream b = new ByteArrayInputStream("d8:announce27:http://www.test.ro/announce7:comment12:test comment10:created by13:uTorrent/177013:creation datei1209116668e8:encoding5:UTF-84:infod6:lengthi85e4:name11:manifest.mf12:piece lengthi65536e6:pieces20:12345678901234567890ee".getBytes(encoding));
+        Torrent info = new Torrent(b, ".", null, 0);
+        b.close();
+        peer.setTorrent(info);
+        MessageValidator v = new MessageValidator(null, protocol);
+        BlockMessage bm = new BlockMessage(0, 0, info.getPieceLength(),
+                SimpleMessage.TYPE_REQUEST, peer);
+        assert !v.validateRequestMessage(bm);
     }
 }
