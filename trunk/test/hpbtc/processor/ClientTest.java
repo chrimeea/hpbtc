232d4d85718604d34d501630de59f7aab29ceb99
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-09-27 17:03:55 +0000

    tests

--- trunk/test/hpbtc/processor/ClientTest.java
+++ trunk/test/hpbtc/processor/ClientTest.java
@@ -5,11 +5,13 @@
 package hpbtc.processor;
 
 import hpbtc.protocol.message.HandshakeMessage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
 import org.junit.Test;
 import util.TorrentUtil;
 
@@ -19,6 +21,8 @@ import util.TorrentUtil;
  */
 public class ClientTest {
 
+    private String byteEncoding = "US-ASCII";
+    
     @Test
     public void testStartStop() throws UnsupportedEncodingException,
             IOException {
@@ -38,10 +42,12 @@ public class ClientTest {
         } catch (IOException e) {}
     }
     
-    @Test
-    public void download() throws UnsupportedEncodingException, IOException {
-        final Client c = new Client();
-        final int port = c.startProtocol();
-        c.stopProtocol();
-    }
+//    @Test
+//    public void download() throws UnsupportedEncodingException, IOException,
+//            NoSuchAlgorithmException {
+//        final Client c = new Client();
+//        c.startProtocol();
+//        c.download(new ByteArrayInputStream("TODO".getBytes(byteEncoding)), ".");
+//        c.stopProtocol();
+//    }
 }
--- trunk/test/hpbtc/protocol/torrent/TrackerTest.java
+++ trunk/test/hpbtc/protocol/torrent/TrackerTest.java
@@ -1,12 +1,8 @@
 package hpbtc.protocol.torrent;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -21,27 +17,13 @@ public class TrackerTest {
     private String byteEncoding = "US-ASCII";
 
     @Test
-    public void testConnectToTracker() throws IOException {
+    public void testConnectToTracker() throws IOException, URISyntaxException {
         final HttpServer server = HttpServer.create(new InetSocketAddress(8000),
                 0);
-        server.createContext("/test", new HttpHandler() {
-
-            public void handle(HttpExchange t) throws IOException {
-                try {
-                    assert t.getRequestURI().equals(
-                            new URI(
-                            "/test?info_hash=INFOHASH&peer_id=PID&port=2000&uploaded=1&downloaded=2&left=3&compact=0&event=started"));
-                } catch (URISyntaxException e) {
-                    assert false;
-                }
-                final String response =
-                        "d8:intervali10e12:min intervali5e10:tracker id3:foo8:completei20e10:incompletei9e5:peersld7:peer id2:1P2:ip9:localhost4:porti9000eed7:peer id2:2P2:ip9:localhost4:porti3003eeee";
-                t.sendResponseHeaders(200, response.length());
-                final OutputStream os = t.getResponseBody();
-                os.write(response.getBytes(byteEncoding));
-                os.close();
-            }
-        });
+        server.createContext("/test", new HttpHandlerStub(
+                "/test?info_hash=INFOHASH&peer_id=PID&port=2000&uploaded=1&downloaded=2&left=3&compact=0&event=started",
+                "d8:intervali10e12:min intervali5e10:tracker id3:foo8:completei20e10:incompletei9e5:peersld7:peer id2:1P2:ip9:localhost4:porti9000eed7:peer id2:2P2:ip9:localhost4:porti3003eeee",
+                byteEncoding));
         server.setExecutor(null);
         server.start();
         final LinkedList<LinkedList<byte[]>> t =
@@ -53,8 +35,7 @@ public class TrackerTest {
         final Tracker ti = new Tracker("INFOHASH".getBytes(byteEncoding),
                 "PID".getBytes(byteEncoding), 2000, t, byteEncoding);
         final Iterable<Peer> peers = ti.updateTracker(Tracker.Event.started, 1,
-                2, 3,
-                false);
+                2, 3, false);
         server.stop(0);
         assert ti.getInterval() == 10;
         assert ti.getMinInterval() == 5;
