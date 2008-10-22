654edecc3921c29224bed655031e15700d7a9344
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2008-10-22 16:56:18 +0000

    dht

--- src/hpbtc/protocol/dht/Network.java
+++ src/hpbtc/protocol/dht/Network.java
@@ -8,7 +8,6 @@ import hpbtc.protocol.network.NetworkLoop;
 import hpbtc.protocol.network.Register;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -32,14 +31,13 @@ public class Network extends NetworkLoop {
 
     private DatagramSocket socket;
     private DatagramPacket packet = new DatagramPacket(new byte[16384], 16384);
-    private RoutingTable table = new RoutingTable();
     private List<DatagramPacket> messagesToSend = Collections.synchronizedList(
             new LinkedList<DatagramPacket>());
-    private String byteEncoding = "US-ASCII";
-    private Map<SocketAddress, Node> nodes = new HashMap<SocketAddress, Node>();
+    private Processor processor;
 
-    public Network(final Register register) {
+    public Network(final Register register, final Processor processor) {
         super(register);
+        this.processor = processor;
     }
 
     public void connect(final int port) throws IOException {
@@ -74,8 +72,8 @@ public class Network extends NetworkLoop {
             BencodingReader reader = new BencodingReader(
                     new ByteArrayInputStream(packet.getData(),
                     packet.getOffset(), packet.getLength()));
-            processMessage(reader.readNextDictionary(),
-                    nodes.get(packet.getAddress()));
+            processor.processMessage(reader.readNextDictionary(),
+                    packet.getAddress());
         } else if (key.isWritable()) {
             if (!messagesToSend.isEmpty()) {
                 socket.send(messagesToSend.get(0));
@@ -83,32 +81,6 @@ public class Network extends NetworkLoop {
                 register.registerNow(socket.getChannel(), selector,
                         SelectionKey.OP_READ, null);
             }
-        }
-    }
-    
-    private void processMessage(final Map<byte[], Object> message,
-            final Node node) throws UnsupportedEncodingException, IOException {
-        final byte[] mid = (byte[]) message.get("t".getBytes(byteEncoding));
-        final char mtype =
-                (char)((byte[]) message.get("y".getBytes(byteEncoding)))[0];
-        switch (mtype) {
-            case 'q':
-                final byte[] mquery =
-                        (byte[]) message.get("q".getBytes(byteEncoding));
-                final Map<byte[], Object> margs = (Map<byte[], Object>)
-                        message.get("a".getBytes(byteEncoding));
-                break;
-            case 'r':
-                final Map<byte[], Object> mret = (Map<byte[], Object>)
-                        message.get("r".getBytes(byteEncoding));
-                break;
-            case 'e':
-                final List<Object> merr = (List<Object>)
-                        message.get("e".getBytes(byteEncoding));
-                break;
-            default:
-                nodes.remove(node);
-                node.disconnect();
         }
     }
 }
