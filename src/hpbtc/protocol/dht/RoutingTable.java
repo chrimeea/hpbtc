1be2787c0d7aee91feb105d5633a2202b799f2a4
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2009-06-02 15:59:36 +0000

    removed peer from node fixed insertnode

--- src/hpbtc/protocol/dht/RoutingTable.java
+++ src/hpbtc/protocol/dht/RoutingTable.java
@@ -5,6 +5,8 @@ package hpbtc.protocol.dht;
 
 import hpbtc.util.ByteStringComparator;
 import hpbtc.util.DHTUtil;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -33,49 +35,47 @@ public class RoutingTable {
     public byte[] getNodeID() {
         return nodeID;
     }
-    
-    private Bucket[] insertNode(final DHTNode node) {
-        int s = table.size();
-        int pos = s / 2;
+
+    private Bucket findBucket(final DHTNode node, int minpos, int maxpos) {
+        int pos = minpos + (maxpos - minpos) / 2;
         Bucket b = table.get(pos);
-        byte[] nid = node.getId();
-        int r = bsc.compare(b.getMax(), nid);
-        while (r >= 0 || bsc.compare(b.getMin(), nid) < 0) {
-            if (r < 0) {
-                pos = pos / 2;
-            } else {
-                pos = (pos + s) / 2;
-            }
-            b = table.get(pos);
-            r = bsc.compare(b.getMax(), nid);
-        }
-        if (!b.insertNode(node) && shouldContain(b, nodeID)) {
-            Bucket[] t = new Bucket[2];
-            byte[] d = DHTUtil.divideByTwo(b.getMax());
-            t[0] = new Bucket(b.getMin(), d);
-            t[1] = new Bucket(d, b.getMax());
-            for (DHTNode i : b.getNodes()) {
-                if (bsc.compare(i.getId(), d) < 0) {
-                    t[0].insertNode(i);
-                } else {
-                    t[1].insertNode(i);
-                }
-            }
-            if (shouldContain(t[0], nid)) {
-                t[0].insertNode(node);
-            } else {
-                t[1].insertNode(node);
-            }
-            table.remove(pos);
-            table.add(pos, t[1]);
-            table.add(pos, t[0]);
-            return t;
+        byte[] id = node.getId();
+        if (bsc.compare(b.getMin(), id) > 0) {
+            return findBucket(node, minpos, pos - 1);
+        } else if (bsc.compare(b.getMax(), id) <= 0) {
+            return findBucket(node, pos + 1, maxpos);
         } else {
-            return new Bucket[] {b};
+            return b;
         }
     }
 
-    private static boolean shouldContain(final Bucket b, final byte[] n) {
-        return bsc.compare(b.getMin(), n) <= 0 && bsc.compare(b.getMax(), n) > 0;
+    private void insertNode(final DHTNode node) {
+        Bucket b = findBucket(node, 0, table.size());
+        if (!b.insertNode(node)) {
+            byte[] d = DHTUtil.divideByTwo(b.getMax());
+            Bucket b1 = new Bucket(b.getMin(), d);
+            Bucket b2 = new Bucket(d, b.getMax());
+            for (DHTNode i : b.getNodes()) {
+                if (bsc.compare(i.getId(), d) < 0) {
+                    b1.insertNode(i);
+                } else {
+                    b2.insertNode(i);
+                }
+            }
+            if (bsc.compare(b1.getMax(), node.getId()) < 0) {
+                b1.insertNode(node);
+            } else {
+                b2.insertNode(node);
+            }
+            int pos = Collections.binarySearch(table, b, new Comparator<Bucket>() {
+
+                public int compare(Bucket one, Bucket two) {
+                    return bsc.compare(one.getMin(), two.getMin());
+                }
+            });
+            table.remove(pos);
+            table.add(pos, b2);
+            table.add(pos, b1);
+        }
     }
 }
