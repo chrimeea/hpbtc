4f68d728618ac6709ddd2ef2bd7946927d3f69fa
chrimeea <chrimeea@5bb17c81-fc0f-0410-a191-85c757bd489f>
2009-07-31 18:59:18 +0000

    refactoring

--- test/hpbtc/protocol/torrent/PeerTest.java
+++ test/hpbtc/protocol/torrent/PeerTest.java
@@ -92,46 +92,4 @@ public class PeerTest {
         p.cancelPieceMessage(1, 2, 3);
         assert p.isMessagesToSendEmpty();
     }
-
-    @Test
-    public void testUploadDownload() throws IOException,
-            NoSuchAlgorithmException, InvalidPeerException {
-        final InetSocketAddress a = new InetSocketAddress(
-                InetAddress.getByName("127.0.0.1"), 6001);
-        final Peer p = new Peer(a);
-        final ByteArrayInputStream bai =
-                new ByteArrayInputStream("d8:announce27:http://www.test.ro/announce7:comment12:test comment10:created by13:uTorrent/177013:creation datei1209116668e8:encoding5:UTF-84:infod6:lengthi85e4:name11:manifest.mf12:piece lengthi65536e6:pieces20:12345678901234567890ee".
-                getBytes(byteEncoding));
-        final Torrent info = new Torrent(bai, ".", null, 0);
-        bai.close();
-        p.setTorrent(info);
-        final ServerSocket ss = new ServerSocket(6001);
-        final byte[] b = "test".getBytes("US-ASCII");
-        final byte[] x = "response".getBytes("US-ASCII");
-        if (!p.connect()) {
-            SocketChannel c = (SocketChannel) p.getChannel();
-            c.finishConnect();
-            c.configureBlocking(false);
-        }
-        assert p.countUploaded() == 0;
-        int i = p.upload(ByteBuffer.wrap(b));
-        assert i == 4;
-        assert p.countUploaded() == 4;
-        p.setNextDataExpectation(8);
-        assert p.countDownloaded() == 0;
-        try {
-            final Socket socket = ss.accept();
-            byte[] r = new byte[4];
-            socket.getInputStream().read(r);
-            assert Arrays.equals(b, r);
-            socket.getOutputStream().write(x);
-            socket.close();
-        } catch (IOException ex) {
-            assert false;
-        }
-        assert p.download();
-        assert p.countDownloaded() == 8;
-        assert Arrays.equals(p.getData().array(), x);
-        ss.close();
-    }
 }
